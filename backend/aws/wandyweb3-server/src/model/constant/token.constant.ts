a369b291396846bf8ea066b09bc54d559148952a
yuexuan <andyliu891009@gmail.com>
2023-02-19 21:18:45 +0800

    init dev and aws api

---
+++ backend/aws/wandyweb3-server/src/model/constant/token.constant.ts
@@ -0,0 +1,11 @@
+export const SOLANA_TOKEN = '2cEqaNLJritepELmaKq1GEujt9onZzDcrXrKoD8sPZop';
+export const SOLANA_TOKEN_MNEMONIC =
+  'cave number march toy hat ranch predict enforce rent evidence when salt';
+
+export const PINATA_API_KEY = '68f08f473c9f89e4cdfb';
+export const PINATA_SECRET_API_KEY =
+  '487ebb283253bbfd0da75bc23c618886d1ad91c5966d14ce158f1d7185b064ae';
+
+export const EVM_AIRDROP_MNEMONIC =
+  'cave number march toy hat ranch predict enforce rent evidence when salt';
+export const EVM_AIRDROP_TOKEN = '0x9A68fB8Dc649E31da2a562A0cb1C0137A96dD68E';
